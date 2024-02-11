02b438526c714a0f0e28ddd423efc1ae349d11be
Jaeyong Shim <hello@beerjoa.dev>
2024-02-10 23:41:11 -0800

    build: add redux, axios and twemoji

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@
 
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -17,5 +18,14 @@ export default defineConfig({
     globals: true,
     environment: 'jsdom',
     setupFiles: ['./src/setupTests.ts'],
+    alias: [
+      {
+        find: 'react-redux/es/exports',
+        replacement: path.resolve(
+          __dirname,
+          './node_modules/react-redux/lib/exports',
+        ),
+      },
+    ],
   },
 });
