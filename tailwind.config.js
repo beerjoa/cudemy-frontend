001bcba6f0bc7a58d1c1f5395f577137cad87a03
Jaeyong Shim <hello@beerjoa.dev>
2024-02-19 22:49:16 -0800

    build: add luxon

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
 /*eslint-env node*/
 /** @type {import('tailwindcss').Config} */
 export default {
@@ -14,7 +15,88 @@ export default {
     },
   },
   daisyui: {
-    themes: ['dark', 'light'],
+    themes: [
+      {
+        light: {
+          primary: '#4f4330',
+          'primary-focus': '#403625',
+          'primary-content': '#f5f3ef',
+
+          secondary: '#c5b69b',
+          'secondary-focus': '#b0a488',
+          'secondary-content': '#f5f3ef',
+
+          accent: '#887149',
+          'accent-focus': '#755f3e',
+          'accent-content': '#f5f3ef',
+
+          neutral: '#3b424e',
+          'neutral-focus': '#2a2e37',
+          'neutral-content': '#f5f3ef',
+
+          'base-100': '#f5f3ef',
+          'base-200': '#ebe9e4',
+          'base-300': '#e0ddd8',
+          'base-content': '#12100d',
+
+          info: '#66c7ff',
+          success: '#87cf3a',
+          warning: '#e1d460',
+          error: '#ff6b6b',
+
+          '--rounded-box': '1rem',
+          '--rounded-btn': '.5rem',
+          '--rounded-badge': '1.9rem',
+
+          '--animation-btn': '.25s',
+          '--animation-input': '.2s',
+
+          '--btn-text-case': 'uppercase',
+          '--navbar-padding': '.5rem',
+          '--border-btn': '1px',
+        },
+      },
+      {
+        dark: {
+          primary: '#cfc3b0',
+          'primary-focus': '#b8ad9e',
+          'primary-content': '#100e0a',
+
+          secondary: '#64553a',
+          'secondary-focus': '#57492f',
+          'secondary-content': '#100e0a',
+
+          accent: '#b69f77',
+          'accent-focus': '#9f8964',
+          'accent-content': '#100e0a',
+
+          neutral: '#2a2e37',
+          'neutral-focus': '#16181d',
+          'neutral-content': '#f2f0ed',
+
+          'base-100': '#100e0a',
+          'base-200': '#1a1814',
+          'base-300': '#24211d',
+          'base-content': '#f2f0ed',
+
+          info: '#66c7ff',
+          success: '#87cf3a',
+          warning: '#e1d460',
+          error: '#ff6b6b',
+
+          '--rounded-box': '1rem',
+          '--rounded-btn': '.5rem',
+          '--rounded-badge': '1.9rem',
+
+          '--animation-btn': '.25s',
+          '--animation-input': '.2s',
+
+          '--btn-text-case': 'uppercase',
+          '--navbar-padding': '.5rem',
+          '--border-btn': '1px',
+        },
+      },
+    ],
     darkTheme: 'dark',
     styled: true,
     utils: true,
